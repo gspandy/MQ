cea7bed4454a25257b22c5455a11308b618000be
wangjun <wangjuneng@126.com>
2016-03-18 22:20:18 +0800

    add Semaphore

---
+++ zmq-remoting/src/main/java/com/ztesoft/zsmart/zmq/remoting/netty/NettyRequestProcessor.java
@@ -0,0 +1,9 @@
+package com.ztesoft.zsmart.zmq.remoting.netty;
+
+import com.ztesoft.zsmart.zmq.remoting.protocol.RemotingCommand;
+
+import io.netty.channel.ChannelHandlerContext;
+
+public interface NettyRequestProcessor {
+	RemotingCommand processRequest(ChannelHandlerContext ctx, RemotingCommand request) throws Exception;
+}
